50d9f685774c5aa02e5ede7810603dab5ed9455d
yunhsuanma <a83mp6vm0@gmail.com>
2024-12-18 15:12:46 +0800

    將carousel改使用owl的(boostrap的還在)，刪除原始faq

--- js/custom.js
+++ js/custom.js
@@ -1,53 +1,102 @@
 
-  (function ($) {
-  
+(function ($) {
+
   "use strict";
 
-    // MENU
-    $('.navbar-collapse a').on('click',function(){
-      $(".navbar-collapse").collapse('hide');
-    });
-    
-    // CUSTOM LINK
-    $('.smoothscroll').click(function(){
-      var el = $(this).attr('href');
-      var elWrapped = $(el);
-      var header_height = $('.navbar').height();
-  
-      scrollToDiv(elWrapped,header_height);
-      return false;
-  
-      function scrollToDiv(element,navheight){
-        var offset = element.offset();
-        var offsetTop = offset.top;
-        var totalScroll = offsetTop-navheight;
-  
-        $('body,html').animate({
+  // MENU
+  $('.navbar-collapse a').on('click', function () {
+    $(".navbar-collapse").collapse('hide');
+  });
+
+  // CUSTOM LINK
+  $('.smoothscroll').click(function () {
+    var el = $(this).attr('href');
+    var elWrapped = $(el);
+    var header_height = $('.navbar').height();
+
+    scrollToDiv(elWrapped, header_height);
+    return false;
+
+    function scrollToDiv(element, navheight) {
+      var offset = element.offset();
+      var offsetTop = offset.top;
+      var totalScroll = offsetTop - navheight;
+
+      $('body,html').animate({
         scrollTop: totalScroll
-        }, 300);
+      }, 300);
+    }
+  });
+
+  $(window).on('scroll', function () {
+    function isScrollIntoView(elem, index) {
+      var docViewTop = $(window).scrollTop();
+      var docViewBottom = docViewTop + $(window).height();
+      var elemTop = $(elem).offset().top;
+      var elemBottom = elemTop + $(window).height() * .5;
+      if (elemBottom <= docViewBottom && elemTop >= docViewTop) {
+        $(elem).addClass('active');
       }
-    });
-
-    $(window).on('scroll', function(){
-      function isScrollIntoView(elem, index) {
-        var docViewTop = $(window).scrollTop();
-        var docViewBottom = docViewTop + $(window).height();
-        var elemTop = $(elem).offset().top;
-        var elemBottom = elemTop + $(window).height()*.5;
-        if(elemBottom <= docViewBottom && elemTop >= docViewTop) {
-          $(elem).addClass('active');
-        }
-        if(!(elemBottom <= docViewBottom)) {
-          $(elem).removeClass('active');
-        }
-        var MainTimelineContainer = $('#vertical-scrollable-timeline')[0];
-        var MainTimelineContainerBottom = MainTimelineContainer.getBoundingClientRect().bottom - $(window).height()*.5;
-        $(MainTimelineContainer).find('.inner').css('height',MainTimelineContainerBottom+'px');
+      if (!(elemBottom <= docViewBottom)) {
+        $(elem).removeClass('active');
       }
-      var timeline = $('#vertical-scrollable-timeline li');
-      Array.from(timeline).forEach(isScrollIntoView);
-    });
-  
-  })(window.jQuery);
+      var MainTimelineContainer = $('#vertical-scrollable-timeline')[0];
+      var MainTimelineContainerBottom = MainTimelineContainer.getBoundingClientRect().bottom - $(window).height() * .5;
+      $(MainTimelineContainer).find('.inner').css('height', MainTimelineContainerBottom + 'px');
+    }
+    var timeline = $('#vertical-scrollable-timeline li');
+    Array.from(timeline).forEach(isScrollIntoView);
+  });
 
+})(window.jQuery);
 
+$(document).ready(function () {
+
+  // owl carousel的設定--作品區
+  $('.first-carousel').owlCarousel({
+    loop: true,
+    margin: 24,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 4
+      }
+    }
+  })
+  $('.second-carousel').owlCarousel({
+    loop: true,
+    margin: 24,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      }
+    }
+  })
+  // owl carousel的設定--精彩案例區
+
+  $('.works-carousel').owlCarousel({
+    items: 1,
+    lazyLoad: true,
+    loop: true,
+    autoplay: true,        // 啟用自動輪播
+    autoplayTimeout: 5200, // 自動輪播間隔時間 (單位: 毫秒)
+    autoplayHoverPause: true,// 滑鼠懸停時暫停自動輪播
+    autoplaySpeed: 1000,// 切換動畫的速度,默認值為 1000 毫秒 
+    smartSpeed: 800, //調整動畫過渡速度
+
+  })
+
+})
